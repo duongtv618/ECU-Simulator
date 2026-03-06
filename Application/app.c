0bc1febe1035c93dedafaf88bddd93ed24d53990
Duong <duongtv618@gmail.com>
2026-03-06 23:00:49 +0900

    add __ipc__region__size__label and add to task param

--- Application/app.c
+++ Application/app.c
@@ -11,7 +11,9 @@
 #define FLASH_SIZE 0x080000U
 #endif
 
-extern uint32_t __ipc_region_start__;
+extern uint32_t __ipc_region_start__[];
+extern uint32_t __ipc_region_end__[];
+extern uint32_t __ipc_region_size__[];
 
 /**
  * Global variable definitions
@@ -19,18 +21,22 @@ extern uint32_t __ipc_region_start__;
 struct sensor_data_s g_sensor_data IPC_REGION;
 
 /** Private variable */
+/** Task stack, need to aligne because of MPU port */
 StackType_t sensor_task_stack[APP_SENSOR_TASK_STACK_SIZE] __attribute__((aligned(APP_SENSOR_TASK_STACK_SIZE * sizeof(StackType_t))));
 StackType_t comm_task_stack[APP_COMM_TASK_STACK_SIZE] __attribute__((aligned(APP_COMM_TASK_STACK_SIZE * sizeof(StackType_t))));
 StackType_t supervisor_task_stack[APP_SUPERVISOR_TASK_STACK_SIZE] __attribute__((aligned(APP_SUPERVISOR_TASK_STACK_SIZE * sizeof(StackType_t))));
 
+/** Pointer to TCB */
 TaskHandle_t sensor_task_handle = NULL;
 TaskHandle_t comm_task_handle = NULL;
 TaskHandle_t supervisor_task_handle = NULL;
 
+/** Static TCB */
 StaticTask_t sensor_task_buffer;
 StaticTask_t comm_task_buffer;
 StaticTask_t supervisor_task_buffer;
 
+/** Task param */
 TaskParameters_t sensor_task_params = {
     .pvTaskCode = sensor_task,
     .pcName = "SensorTask",
@@ -42,7 +48,7 @@ TaskParameters_t sensor_task_params = {
     .xRegions = {
         {sensor_task_stack, sizeof(sensor_task_stack) * sizeof(StackType_t), portMPU_REGION_READ_WRITE},
         {(void *)FLASH_BASE, FLASH_SIZE, portMPU_REGION_READ_ONLY},
-        {(void *)0x2001F000, 0x1000, portMPU_REGION_READ_WRITE}}};
+        {(void *)__ipc_region_start__, (uint32_t)__ipc_region_size__, portMPU_REGION_READ_WRITE}}};
 
 TaskParameters_t comm_task_params = {
     .pvTaskCode = comm_task,
@@ -55,7 +61,7 @@ TaskParameters_t comm_task_params = {
     .xRegions = {
         {comm_task_stack, sizeof(comm_task_stack) * sizeof(StackType_t), portMPU_REGION_READ_WRITE},
         {(void *)FLASH_BASE, FLASH_SIZE, portMPU_REGION_READ_ONLY},
-        {(void *)0x2001F000, 0x1000, portMPU_REGION_READ_WRITE}}};
+        {(void *)__ipc_region_start__, (uint32_t)__ipc_region_size__, portMPU_REGION_READ_WRITE}}};
 
 TaskParameters_t supervisor_task_params = {
     .pvTaskCode = supervisor_task,
@@ -68,32 +74,39 @@ TaskParameters_t supervisor_task_params = {
     .xRegions = {
         {supervisor_task_stack, sizeof(supervisor_task_stack) * sizeof(StackType_t), portMPU_REGION_READ_WRITE},
         {(void *)FLASH_BASE, FLASH_SIZE, portMPU_REGION_READ_ONLY},
-        {(void *)0x2001F000, 0x1000, portMPU_REGION_READ_WRITE}}};
+        {(void *)__ipc_region_start__, (uint32_t)__ipc_region_size__ , portMPU_REGION_READ_WRITE}}};
 
 /** Buffer for queue, sem ... */
 static StaticSemaphore_t g_sensor_data_mutex;
 static uint16_t adc_queue_buffer[10];
 static StaticQueue_t g_adc_queue_buffer;
 
+/**
+ * @brief Init application
+ * 
+ */
 void app_init(void)
 {
-    HAL_Init();
-    MX_GPIO_Init();
-    MX_USART2_UART_Init();
-    MX_ADC1_Init();
-    // MX_IWDG_Init();
-    MX_TIM3_Init();
-    xTaskCreateRestrictedStatic(&sensor_task_params, &sensor_task_handle);
-
-    xTaskCreateRestrictedStatic(&comm_task_params, &comm_task_handle);
-
-    xTaskCreateRestrictedStatic(&supervisor_task_params, &supervisor_task_handle);
-
+    /** First is os */
+    os_init();
+    
     // Initialize application resources, tasks, and synchronization primitives here
     g_sensor_data.data_mutex = xSemaphoreCreateMutexStatic(&g_sensor_data_mutex);
     g_adc_queue = xQueueCreateStatic(10U, sizeof(uint16_t), (uint8_t *)adc_queue_buffer, &g_adc_queue_buffer);
+
+    /** Then task create */
+    xTaskCreateRestrictedStatic(&sensor_task_params, &sensor_task_handle);
+    xTaskCreateRestrictedStatic(&comm_task_params, &comm_task_handle);
+    xTaskCreateRestrictedStatic(&supervisor_task_params, &supervisor_task_handle);
+
+    /** Register task for jitter measuring */
+    os_registerJitterTask(comm_task_handle, APP_COMM_TASK_PERIOD_MS);
+    os_registerJitterTask(supervisor_task_handle, APP_SUPERVISOR_TASK_PERIOD_MS);
+    os_registerJitterTask(sensor_task_handle, APP_SENSOR_TASK_PERIOD_MS);
+
 }
 
+/** Start the app */
 void app_start(void)
 {
     adc_start_sampling();
