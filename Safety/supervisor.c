0c7476b3df25fada1b66fc2f1d55a3f7a4e64413
Duong <duongtv618@gmail.com>
2026-03-08 16:14:01 +0900

    change alive check technique

--- Safety/supervisor.c
+++ Safety/supervisor.c
@@ -4,35 +4,25 @@
 
 #define BITS_TO_WAIT (APP_SENSOR_TASK_HB_BIT | APP_CONTROL_TASK_HB_BIT)
 
-extern EventGroupHandle_t g_hb_evengroup;
+extern uint32_t g_alive;
 /**
  * @brief Supervisor task main function
- * 
- * @param pvParameters 
+ *
+ * @param pvParameters
  */
 void supervisor_task(void *pvParameters)
 {
     (void)pvParameters;
-    // TickType_t xLastWakeTime;
-
+    TickType_t lastTick = xTaskGetTickCount();
     for (;;)
     {
-        // xLastWakeTime = xTaskGetTickCount();
-        // iwdg_refresh();
-        /* Wait for the next cycle */
-        EventBits_t retBit = xEventGroupWaitBits(g_hb_evengroup, BITS_TO_WAIT, pdTRUE, pdTRUE, pdMS_TO_TICKS(APP_SUPERVISOR_TASK_PERIOD_MS));
-
-        if ((retBit & BITS_TO_WAIT) == BITS_TO_WAIT) {
-            //System ok
-        } else if ((retBit & APP_SENSOR_TASK_HB_BIT) == 0) {
-            //Sensor task fail
-            while(1);
-        } else if ((retBit & APP_CONTROL_TASK_HB_BIT) == 0) {
-            //Control task fail
-            while(1);
+        if ((g_alive & BITS_TO_WAIT) == BITS_TO_WAIT){
+            //System healthy
+            g_alive = 0;
         } else {
-            //Both are not being set
-            while(1);
+            while (1);
         }
+
+        vTaskDelayUntil(&lastTick, pdMS_TO_TICKS(APP_SUPERVISOR_TASK_PERIOD_MS));
     }
 }
