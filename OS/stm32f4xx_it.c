60ffe8b3589082fb6823bae971adabf38ee21f64
Duong <duongtv618@gmail.com>
2026-03-10 22:55:12 +0900

    add log stack frame on hardfault

--- OS/stm32f4xx_it.c
+++ OS/stm32f4xx_it.c
@@ -22,10 +22,27 @@
 #include "stm32f4xx_it.h"
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
+#include <string.h>
+#include "os.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
 /* USER CODE BEGIN TD */
+typedef struct log_fault_s
+{
+  uint32_t cfsr;
+  uint32_t hfsr;
+  uint32_t mmfar;
+  uint32_t bfar;
+  uint32_t pc;
+  uint32_t lr;
+  uint32_t xPSR;
+  uint32_t r12;
+  uint32_t r0;
+  uint32_t r1;
+  uint32_t r2;
+  uint32_t r3;
+} fault_log_t;
 
 /* USER CODE END TD */
 
@@ -41,17 +58,114 @@
 
 /* Private variables ---------------------------------------------------------*/
 /* USER CODE BEGIN PV */
+const char log_str_r0[] = "R0:\t0x";
+const char log_str_r1[] = "R1:\t0x";
+const char log_str_r2[] = "R2:\t0x";
+const char log_str_r3[] = "R3:\t0x";
+const char log_str_r12[] = "R12:\t0x";
+const char log_str_lr[] = "LR:\t0x";
+const char log_str_pc[] = "PC:\t0x";
+const char log_str_xPSR[] = "xPSR:\t0x";
+const char log_str_cfsr[] = "CFSR:\t0x";
+const char log_str_hfsr[] = "HFSR:\t0x";
+const char log_str_mmfar[] = "MMFAR:\t0x";
+const char log_str_bfar[] = "BFAR:\t0x";
 
+static fault_log_t s_fault;
+static char log_msg_buf[20];
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
 /* USER CODE BEGIN PFP */
-
+static void hard_fault_handler(uint32_t *stack);
+static void uart2_write_polling(const char *buf, uint16_t len);
+static void inline write_ln(void);
 /* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
+static void inline write_ln(void)
+{
+  uart2_write_polling("\r\n", strlen("\r\n"));
+}
 
+/**
+ * @brief Write data to uart2 with polling technique, DMA can be broken now
+ * 
+ * @param buf Buffer to write
+ * @param len Length of buffer
+ */
+static void uart2_write_polling(const char *buf, uint16_t len)
+{
+  for (int i = 0; i < len; i++)
+  {
+    while (!(USART2->SR & USART_SR_TXE))
+      ;
+    USART2->DR = buf[i];
+  }
+}
+
+/**
+ * @brief Hardfault handler with current stack address
+ * 
+ * @param stack Stack address
+ */
+static void hard_fault_handler(uint32_t *stack)
+{
+  /** Take stack frame */
+  s_fault.r0 = stack[0];
+  s_fault.r1 = stack[1];
+  s_fault.r2 = stack[2];
+  s_fault.r3 = stack[3];
+  s_fault.r12 = stack[4];
+  s_fault.lr = stack[5];
+  s_fault.pc = stack[6];
+  s_fault.xPSR = stack[7];
+
+  /** Take control function register */
+  s_fault.cfsr = SCB->CFSR;
+  s_fault.hfsr = SCB->HFSR;
+  s_fault.mmfar = SCB->MMFAR;
+  s_fault.bfar = SCB->BFAR;
+
+  /** Log to uart */
+  uart2_write_polling(log_str_r0, strlen(log_str_r0));
+  uart2_write_polling(os_itoa(s_fault.r0, log_msg_buf, 16), strlen(log_msg_buf));
+  write_ln();
+  uart2_write_polling(log_str_r1, strlen(log_str_r1));
+  uart2_write_polling(os_itoa(s_fault.r1, log_msg_buf, 16), strlen(log_msg_buf));
+  write_ln();
+  uart2_write_polling(log_str_r2, strlen(log_str_r2));
+  uart2_write_polling(os_itoa(s_fault.r2, log_msg_buf, 16), strlen(log_msg_buf));
+  write_ln();
+  uart2_write_polling(log_str_r12, strlen(log_str_r12));
+  uart2_write_polling(os_itoa(s_fault.r12, log_msg_buf, 16), strlen(log_msg_buf));
+  write_ln();
+  uart2_write_polling(log_str_lr, strlen(log_str_lr));
+  uart2_write_polling(os_itoa(s_fault.lr, log_msg_buf, 16), strlen(log_msg_buf));
+  write_ln();
+  uart2_write_polling(log_str_pc, strlen(log_str_pc));
+  uart2_write_polling(os_itoa(s_fault.pc, log_msg_buf, 16), strlen(log_msg_buf));
+  write_ln();
+  uart2_write_polling(log_str_xPSR, strlen(log_str_xPSR));
+  uart2_write_polling(os_itoa(s_fault.xPSR, log_msg_buf, 16), strlen(log_msg_buf));
+  write_ln();
+  uart2_write_polling(log_str_cfsr, strlen(log_str_cfsr));
+  uart2_write_polling(os_itoa(s_fault.cfsr, log_msg_buf, 16), strlen(log_msg_buf));
+  write_ln();
+  uart2_write_polling(log_str_hfsr, strlen(log_str_hfsr));
+  uart2_write_polling(os_itoa(s_fault.hfsr, log_msg_buf, 16), strlen(log_msg_buf));
+  write_ln();
+  uart2_write_polling(log_str_mmfar, strlen(log_str_mmfar));
+  uart2_write_polling(os_itoa(s_fault.mmfar, log_msg_buf, 16), strlen(log_msg_buf));
+  write_ln();
+  uart2_write_polling(log_str_bfar, strlen(log_str_bfar));
+  uart2_write_polling(os_itoa(s_fault.bfar, log_msg_buf, 16), strlen(log_msg_buf));
+  write_ln();
+
+  /** Keep in loop so that IWDG can reset MCU */
+  while (1);
+}
 /* USER CODE END 0 */
 
 /* External variables --------------------------------------------------------*/
@@ -80,21 +194,22 @@ void NMI_Handler(void)
   /* USER CODE END NonMaskableInt_IRQn 1 */
 }
 
+
+
+
 /**
   * @brief This function handles Hard fault interrupt.
   */
 void HardFault_Handler(void)
 {
   /* USER CODE BEGIN HardFault_IRQn 0 */
-
-  volatile uint32_t fault_address = SCB->MMFAR;
-  volatile uint32_t cfsr = SCB->CFSR;
-  volatile uint32_t mmfsr = (cfsr & 0xFF);
-  volatile uint32_t fault_status = (mmfsr & 0x7F);
-  (void)fault_address;
-  (void)cfsr;
-  (void)mmfsr;
-  (void)fault_status;
+  uint32_t *stack;
+  /** Choose current stack pointer to store stack frame */
+  if (__get_CONTROL() & 2)
+    stack = (uint32_t *)__get_PSP();
+  else
+    stack = (uint32_t *)__get_MSP();
+  hard_fault_handler(stack);
   /* USER CODE END HardFault_IRQn 0 */
   while (1)
   {
@@ -117,9 +232,14 @@ void MemManage_Handler(void)
   (void)cfsr;
   (void)mmfsr;
   (void)fault_status;
+  /** Can log to FLash or RTC register, in Mem Fault unprivileged task has just
+   * access to an inaccessable region, can use msp to work with log function
+   */
+  os_log(OS_DEBUG_CONSOLE, (uint8_t*)"Have memmanage fault\r\n", 0);
   /* USER CODE END MemoryManagement_IRQn 0 */
   while (1)
   {
+    /** Then reset ECU */
     /* USER CODE BEGIN W1_MemoryManagement_IRQn 0 */
     /* USER CODE END W1_MemoryManagement_IRQn 0 */
   }
