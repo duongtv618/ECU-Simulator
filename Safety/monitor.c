f762b2e302cc853c76cddb6ca6b66dcd521522bd
Duong <duongtv618@gmail.com>
2026-03-11 22:36:42 +0900

    syntax error fix

--- Safety/monitor.c
+++ Safety/monitor.c
@@ -11,6 +11,6 @@ void monitor_task(void * pvParameters)
     uint32_t lastTick = xTaskGetTickCount();
     while (1)
     {
-        vTaskDelayUntil(&lastTick, pdMS_TO_TICKS(APP_MONITOR_TASK_PERIOD_MS)):
+        vTaskDelayUntil(&lastTick, pdMS_TO_TICKS(APP_MONITOR_TASK_PERIOD_MS));
     }
 }
